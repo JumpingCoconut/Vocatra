e3bea1eec1c0446299b5eeacc1fa49d40dd9f2d1
fakecora <fakecora@googlemail.com>
2011-10-25 18:41:04 +0200

    Ui Entwurf

--- de.chandelier.vocatra/src/de/chandelier/vocatra/View.java
+++ de.chandelier.vocatra/src/de/chandelier/vocatra/View.java
@@ -1,5 +1,6 @@
 package de.chandelier.vocatra;
 
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
@@ -7,7 +8,10 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ExpandBar;
+import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
@@ -17,61 +21,40 @@ public class View extends ViewPart {
 
 	private TableViewer viewer;
 
-	/**
-	 * The content provider class is responsible for providing objects to the
-	 * view. It can wrap existing objects in adapters or simply return objects
-	 * as-is. These objects may be sensitive to the current input of the view,
-	 * or ignore it and always show the same content (like Task List, for
-	 * example).
-	 */
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-
-		public void dispose() {
-		}
-
-		public Object[] getElements(Object parent) {
-			if (parent instanceof Object[]) {
-				return (Object[]) parent;
-			}
-	        return new Object[0];
-		}
-	}
-
-	class ViewLabelProvider extends LabelProvider implements
-			ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().getSharedImages().getImage(
-					ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
-
-	/**
+		/**
 	 * This is a callback that will allow us to create the viewer and initialize
 	 * it.
 	 */
 	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL
-				| SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		// Provide the input to the ContentProvider
-		viewer.setInput(new String[] {"Vocatra FTW !!"});
+		ExpandBar expandBar = new ExpandBar(parent, SWT.None);
+		ExpandItem vocabularyItem = new ExpandItem(expandBar, SWT.NONE);
+		vocabularyItem.setText("Vocabulary");
+		Composite composite = new Composite(expandBar, SWT.BORDER);
+		composite.setLayout(new FillLayout());
+		viewer = new TableViewer(composite);
+		viewer.setContentProvider(new ArrayContentProvider());
+		viewer.setLabelProvider(new LabelProvider());
+		viewer.setInput(new String[]{"Vokabeln Lektion 1","Vokabeln Lektion 2","Vokabeln Lektion 3"});
+		vocabularyItem.setHeight(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		vocabularyItem.setControl(composite);
+		
+		Composite grammarComposite = new Composite(expandBar, SWT.BORDER);
+		grammarComposite.setLayout(new FillLayout());
+		TableViewer gramarViewer = new TableViewer(grammarComposite);
+		gramarViewer.setContentProvider(new ArrayContentProvider());
+		gramarViewer.setLabelProvider(new LabelProvider());
+		gramarViewer.setInput(new String[]{"Grammatik Lektion 1","Grammatik Lektion 2","Grammatik Lektion 3"});
+		ExpandItem grammarItem = new ExpandItem(expandBar, SWT.NONE);
+		grammarItem.setText("Grammar");
+		grammarItem.setHeight(grammarComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		grammarItem.setControl(grammarComposite);
+		
 	}
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
-		viewer.getControl().setFocus();
+//		viewer.getControl().setFocus();
 	}
 }
